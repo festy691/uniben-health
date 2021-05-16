6a7082c53be519540be8106cbbb43c65a181376b
festy691 <festy691@gmail.com>
2021-05-16 22:08:54 +0100

    minor fix

--- api/resources/session/session.controller.js
+++ api/resources/session/session.controller.js
@@ -61,45 +61,42 @@ module.exports =  {
     async rescheduleSession (req,res){
         try {
 
-            const sess = await UserModel.findOne({_id:req.body.user});
+            const user = await UserModel.findOne({_id:req.body.user});
 
             if(!req.body.user) return res.status(400).send({'error':'User cannot be null'});
             if(!req.body.sessionDate) return res.status(400).send({'error':'Session Date cannot be null'});
             if(!req.body.startTime) return res.status(400).send({'error':'Start Time cannot be null'});
             if(!req.body.endTime) return res.status(400).send({'error':'End Time cannot be null'});
 
-            if(!sess){
+            if(!user){
                 return res.status(404).send({'error':'Session not found'});
             }
 
             const oldSession = await SessionModel.findOne(
                 {
                     user:req.body.user,
-                    sessionDate:req.body.sessionDate
+                    _id:req.params.id
                 });
 
-            if(oldSession.length > 0){
+            if(oldSession){
                 const data = req.body;
                 const startTime = data.startTime;
                 const endTime = data.endTime;
-                const getTime = time => new Date(2021,5,9, time.subString(0,2), time.subString(3,5), 0, 0);
-                for (let i=0; i<oldSession.length; i++){
-                    if((getTime(oldSession[i].startTime) >= getTime(startTime)) && (getTime(oldSession[i].endTime) > getTime(startTime))){
-                        return res.status(404).send({'error':'This time is not free'});
-                    }
-                    if((getTime(oldSession[i].startTime) <= getTime(endTime)) && (getTime(oldSession[i].endTime) >= getTime(endTime))){
-                        return res.status(404).send({'error':'This time is not free'});
-                    }
+                if((getTime(oldSession.startTime) >= getTime(startTime)) && (getTime(oldSession.endTime) > getTime(startTime))){
+                    return res.status(404).send({'error':'This time is not free'});
+                }
+                if((getTime(oldSession.startTime) <= getTime(endTime)) && (getTime(oldSession.endTime) >= getTime(endTime))){
+                    return res.status(404).send({'error':'This time is not free'});
                 }
             }
             
-            if (req.body.startTime) sess.startTime = req.body.startTime;
-            if (req.body.startTime) sess.endTime = req.body.endTime;
-            if (req.body.startTime) sess.sessionDate = req.body.sessionDate;
-            if (req.body.startTime) sess.user = req.body.user;
+            if (req.body.startTime) oldSession.startTime = req.body.startTime;
+            if (req.body.startTime) oldSession.endTime = req.body.endTime;
+            if (req.body.startTime) oldSession.sessionDate = req.body.sessionDate;
+            if (req.body.startTime) oldSession.user = req.body.user;
 
-            await sess.save((err,doc)=>{
-                if(err){
+            await oldSession.save((err,doc)=>{
+                if(err){  
                     return res.status(400).send({'error':err});
                 }
                 else{
@@ -270,6 +267,5 @@ module.exports =  {
     },
 }
 function getTime(time) {
-    console.log("time");
     return new Date("01/01/2000 "+time);
 }
